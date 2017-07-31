f785daa2b2e476945542d5f2f22a12c7fd9464d5
Robert Seymour <Robert Seymour>
2017-07-31 16:23:30 -0400

    added manual line entering and selection storing

--- bear.js
+++ bear.js
@@ -24,12 +24,9 @@ function displayContents(contents) {
   lines = contents.split("\n");
   for (var i = 0; i < lines.length; i++) {
     element.innerHTML +=
-    '<pre class = "clearedLine" onmousedown = "storeNumber(this)" line-number = '+ (i+1) +'>' + (i+1) + '    ' + lines[i] +'</pre>';
-    // '<pre class = "clearedLine" line-number = '+ (i+1) +'>' + (i+1) + '    ' + lines[i] +'</pre>';
+    '<pre class = "clearedLine" line-number = '+ (i+1) +'>' + (i+1) + '    ' + lines[i] +'</pre>';
   }
-
 }
-//document.addEventListener("selectionchange", storeNumber(this));
 
 var el = document.getElementById('file-input');
 if(el){
@@ -45,56 +42,42 @@ function escapeHtml(unsafe) {
   .replace(/'/g, "&#039;");
 }
 
-function storeNumber(line) {
-  // debugger;
-
-  var lineNumber = parseInt(line.getAttribute("line-number"));
-  if(chosenLines.contains(lineNumber) == true) {
-    var index = chosenLines.indexOf(lineNumber);
-    delete chosenLines[index];
-    highlightSingleLine(line, "old");
-    debugger;
-  }
-  else {
-    chosenLines.push(lineNumber);
-    chosenLines = chosenLines.map(Number);
-    highlightSingleLine(line, "new");
-  }
-  // debugger;
-}
-
-Array.prototype.contains = function(obj) {
-  var i = this.length;
-  while (i--) {
-    if (this[i] == obj) {
-      return true;
+function displayArray() {
+  var container = document.getElementById("file-content");
+  var children = container.childNodes;
+  for (var i = 0; i< children.length; i++){
+    var node = children[i];
+    if(node.classList.contains("highlight")){
+      var lineNumber = parseInt(node.getAttribute("line-number"));
+      chosenLines.push(lineNumber);
     }
   }
-  return false;
 }
 
-function displayArray() {
-  alert(chosenLines);
-}
-
-// function storeSecondNumber(line) {
-//   chosenLines.push(line.getAttribute("line-number"));
-//   chosenLines = chosenLines.map(Number);
-//   if(chosenLines[chosenLines.length - 1] != chosenLines[chosenLines.length]) {
-//     for (var i = 0; i < abs(chosenLines[chosenLines.length]-chosenLines[chosenLines.length - 1])){
-//       chosenLines.plice()
+// function storeNumber(line) {
+//   // debugger;
+//
+//   var lineNumber = parseInt(line.getAttribute("line-number"));
+//   if(chosenLines.contains(lineNumber) == true) {
+//     var index = chosenLines.indexOf(lineNumber);
+//     delete chosenLines[index];
+//     highlightSingleLine(line);
+//     debugger;
+//   }
+//   else {
+//     chosenLines.push(lineNumber);
+//     chosenLines = chosenLines.map(Number);
+//     highlightSingleLine(line);
+//   }
+//   // debugger;
+// }
+//
+// Array.prototype.contains = function(obj) {
+//   var i = this.length;
+//   while (i--) {
+//     if (this[i] == obj) {
+//       return true;
 //     }
 //   }
+//   return false;
 // }
-
-// document.getElementById('file-input').addEventListener('change',
-//   function () {
-//     var fr = new FileReader();
-//     fr.onload = function () {
-//       document.getElementById('file-content').textContent = this.result;
-//     };
-//     fr.readAsText(this.files[0]);
-//   }
-//   );
-
-// HELLO
--- highlight.js
+++ highlight.js
@@ -2,35 +2,22 @@ var firstLine;
 var secondLine;
 var singleLines;
 
-function selectTextareaLine(){
-  getLines();
-  if(firstLine == undefined || secondLine == undefined){
-    return 0;
-  }
-  var tarea = document.getElementById('file-content').innerText;
-  tarea = escapeHtml(tarea);
-  var lines = tarea.split("\n");
-  if (lines != undefined && tarea != undefined){
-    for (var i = 0; i < lines.length; i++){
-      if (singleLines.includes(i)) {
-        document.getElementById('line-container').innerHTML +=
-        '<pre class = "highlight" line-number = '+ (i+1) +'>' + (i+1) + '    ' + lines[i] +'</span>';
-      }
-      else if(i >= firstLine && i <= secondLine){
-        document.getElementById('line-container').innerHTML +=
-        '<pre class = "highlight" line-number = '+ (i+1) +'>' + (i+1) + '    ' + lines[i] +'</span>';
-      }
-      else{
-        document.getElementById('line-container').innerHTML +=
-        '<pre class = "clearedLine" line-number = '+ i+1 +'>' + (i+1) + '    ' + lines[i] +'</pre>';
-      }
-    }
-  }
-  else {
-    return;
-  }
-  document.getElementById('file-content').style.display = 'none'
+function selectTextareaLine() {
+  var container = document.getElementById("file-content");
+  var children = container.childNodes;
   debugger;
+  getLines();
+  for (var i = firstLine; i <= secondLine; i++) {
+    var node = children[i];
+    node.classList.remove("highlight");
+    highlightSingleLine(node);
+    debugger;
+  }
+  for(var i = 0; i < singleLines.length; i++) {
+    var val = singleLines[i];
+    debugger;
+    highlightSingleLine(children[val]);
+  }
 }
 
 function getLines(){ //Make sure to account for prefilled
@@ -44,29 +31,55 @@ function getLines(){ //Make sure to account for prefilled
   }
 }
 
-function escapeHtml(unsafe) {
-  return unsafe
-  .replace(/&/g, "&amp;")
-  .replace(/</g, "&lt;")
-  .replace(/>/g, "&gt;")
-  .replace(/"/g, "&quot;")
-  .replace(/'/g, "&#039;");
+function highlightSingleLine(line) {
+  line.classList.add("highlight");
+  line.classList.remove("clearedLine");
 }
 
-function highlightSingleLine(line, arg) {
-  if(arg == "new"){
-    if(line.classList == "clearedLine") {
-      line.classList.add("highlight");
-      line.classList.toggle("clearedLine");
-    }
-    else {
-      line.classList.toggle("highlight");
-    }
-  }
-  else {
-    line.classList.toggle("highlight");
-  }
-  //  line.classList.remove("")
-  //  line.classList.add("highlight");
-  debugger;
-}
+
+// function highlightSingleLine(line, arg) {
+//   if(arg == "new"){
+//     if(line.classList == "clearedLine") {
+//       line.classList.add("highlight");
+//       line.classList.toggle("clearedLine");
+//     }
+//     else {
+//       line.classList.toggle("highlight");
+//     }
+//   }
+//   else {
+//     line.classList.toggle("highlight");
+//   }
+// }
+
+
+// function selectTextareaLine(){
+//   getLines();
+//
+//   if(firstLine == undefined || secondLine == undefined){
+//     return 0;
+//   }
+//   var tarea = document.getElementById('file-content').innerText;
+//   tarea = escapeHtml(tarea);
+//   var lines = tarea.split("\n");
+//   if (lines != undefined && tarea != undefined){
+//     for (var i = 0; i < lines.length; i++){
+//       if (singleLines.includes(i)) {
+//         document.getElementById('line-container').innerHTML +=
+//         '<pre class = "highlight" line-number = '+ (i+1) +'>' + '    ' + lines[i] +'</span>';
+//       }
+//       else if(i >= firstLine && i <= secondLine){
+//         document.getElementById('line-container').innerHTML +=
+//         '<pre class = "highlight" line-number = '+ (i+1) +'>' + '    ' + lines[i] +'</span>';
+//       }
+//       else{
+//         document.getElementById('line-container').innerHTML +=
+//         '<pre class = "clearedLine" line-number = '+ i+1 +'>' + '    ' + lines[i] +'</pre>';
+//       }
+//     }
+//   }
+//   else {
+//     return;
+//   }
+//   document.getElementById('file-content').style.display = 'none';
+// }
